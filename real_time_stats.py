5c2239c746e0cab9851a9e3fe90ed91f08a95798
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-26 15:29:43 +0300

    (feat): Add real_time_stats.py
    This file contains all the functions needed to update the redis DBMS with the data that are being updated in realtime.
    
    These functions are:
    - `def update_player_location(player_id, position):`
        - This method is responsible of updating the player's location (currently we're concerned with x & y position only)
        - Its parameters are:
            - `player_id`: The player's id that we want to update their position.
            - `position`: A dictionary containing the new x & y position.
    - `def get_player_location(player_id):`
        - This method is responsible of retrieving the player's location from the redis DBMS
        - Its parameters are:
            - `player_id`: The player's id we want to retrieve their position.
    - `def update_game_event(event_id, player_id, event_type, details)`
        - This method is responsible of updating the event's details, which are whom triggered the event, what type of event it is, and extra details on the evnet.
        - Its parameters are:
            - `event_id`: The id of the event we wish to update.
            - `player_id`: The player whom triggered the event update.
            - `event_type`: The type of event which can only at the moment be either `item_pickup` or `enemy_defeated`.
            - `details`: Extra details about the evnet like the item id that was picked up or the enemy id that was defeated.
    - `def get_game_event(event_id):`
        - This method is responsible of retrieving the game event's details
        - Its parameters are:
            - `event_id`: The if of the event we want its details retrieved.
    - `def update_world_resource(resource_id, resource,type, quantity, location):`
        - This method is responsible of updating the resource's details.
        - Its parameters are:
            - `resource_id`: The resoruce id we wish to update.
            - `resource_type`: The type of the resource.
            - `quantity`: The quantity of the resource available.
            - `location`: The x & y location of the resource.
    - `def get_world_resource(resource_id):`
        - This method is responsible of retrieving the resource details.
        - Its parameters are:
            - `resource_id`: The resource id we want to retrieve it's details.
    
    Files:
    - real_time_stats.py

---
+++ real_time_stats.py
@@ -0,0 +1,128 @@
+"""
+"""
+
+import json
+import time
+from typing import Dict, Literal
+
+redis = __import__('connection').r
+
+
+def update_player_location(player_id: int, position: Dict[str, int]):
+    """update_player_location
+
+    This method takes in the playerID that their position has changed
+    along with the new x & y position and updates the DBMS with
+    the new position of the player in the world.
+
+    Arguments:
+        player_id (int): The player's id that their position has changed.
+        position (Dict[str, int]): The x & y position of the player.
+    """
+    timestamp = int(time.time())
+    redis.hset(f"game:player:location:{player_id}", mapping={
+        'position': json.dumps(position),
+        'timestamp': timestamp
+    })
+
+
+def get_player_location(player_id: int) -> Dict[str, int]:
+    """get_player_location
+
+    This method retrieves the positions of the player if they're
+    in the system.
+
+    Arguments:
+        player_id (int): The player's id that we want their position retrieved.
+
+    Return:
+        (Dict[str, int]): The player's x & y position along with the
+                               timestamp.
+    """
+    player_location = redis.hgetall(f"game:player:location:{player_id}")
+    if player_location:
+        return player_location
+    return (f"Player ID {player_id} not found")
+
+
+def update_game_event(event_id: int, player_id: int,
+                      event_type: Literal['item_pickup', 'enemy_defeated'],
+                      details: Dict[str, any]) -> int:
+    """update_game_event
+
+    This method notifies the DBMS of an event update, these events could
+    be either the player has picked up an item or has defeated
+    an enemy.
+
+    Arguments:
+        event_id (int): The event's id to be updated.
+        player_id (int): The player's id that caused the event update.
+        event_type (Literal['item_pickup', 'enemy_defeated']): Type of event.
+        details (Dict[str, any]): Additional details about the event.
+    """
+    timestamp = time.time()
+    redis.hset(f"game:event:{event_id}", mapping={
+        'event_type': event_type,
+        'player_id': player_id,
+        'details': json.dumps(details),
+        'timestamp': timestamp
+    })
+
+
+def get_game_event(event_id: int) -> Dict[str, any]:
+    """get_game_event
+
+    This method retrieves the event details for the specific event id
+    if it's in the system.
+
+    Arguments:
+        event_id (int): The event id that we want its details retrieved.
+
+    Return:
+        (Dict[str, any]): The game event details along with the type,
+                          player id and the timestamp of the event.
+    """
+    game_event = redis.hgetall(f"game:event:{event_id}")
+    if game_event:
+        return game_event
+    return (f"Event id {event_id} not found")
+
+
+def update_world_resource(resource_id: str, resource_type: str, quantity: int,
+                          location: Dict[str, int]):
+    """update_world_resource
+
+    This method notifies the DBMS of a resource update.
+
+    Arguments:
+        resource_id (str): The unique identifier of the resource.
+        type (str): The type of the resource (e.g., wood, stone).
+        quantity (int): The current quantity of the resource.
+        location (Dict[str, int]): The location of the resource, represented as
+                                   a dictionary with keys 'x', 'y'.
+    """
+    timestamp = time.time()
+    redis.hset(f"game:world:resource:{resource_id}", mapping={
+        'type': resource_type,
+        'quantity': quantity,
+        'location': json.dumps(location),
+        'timestamp': timestamp
+    })
+
+
+def get_world_resource(resource_id: str) -> Dict[str, any]:
+    """get_world_resource
+
+    This method retrieves the resource details for the specific resource id
+    if it's in the system.
+
+    Arguments:
+        resource_id (str): The resource id that we want its details retrieved.
+
+    Return:
+        (Dict[str, any]): The resource details.
+    """
+    game_event = redis.hgetall(f"game:world:resource:{resource_id}")
+    if game_event:
+        return game_event
+    return (f"Event id {resource_id} not found")
