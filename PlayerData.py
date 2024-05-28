a5b7548dbfde9409d7ebf55c844fff46f85ac700
MalakHalawany <21-101181@students.eui.edu.eg>
2024-05-28 21:34:21 +0300

    Commit message describing your changes

--- PlayerData.py
+++ PlayerData.py
@@ -1,6 +1,4 @@
 from connection import c
-from uuid import uuid4
-
 
 c.set_keyspace("game")
 
@@ -143,8 +141,8 @@ def main():
     print()
 
     # Deleting the player profile
-    print("Deleting player profile...")
-    delete_player_profile(player_id)
-    print(f"Player profile with ID {player_id} deleted successfully.")
+    #print("Deleting player profile...")
+    #delete_player_profile(player_id)
+    #print(f"Player profile with ID {player_id} deleted successfully.")
 
 main()
