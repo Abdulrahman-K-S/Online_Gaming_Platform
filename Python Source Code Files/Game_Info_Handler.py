519cf0a4c4ceb113cc8b5f578e71abc87e34b562
OmarElabasery <omar_elabasery@outlook.com>
2024-05-28 21:09:45 +0300

    Uploading CQL files and Python files of GameInfo and GameObject tables

---
+++ Python Source Code Files/Game_Info_Handler.py
@@ -0,0 +1,50 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+import uuid
+
+# Connect to Cassandra
+cluster = Cluster(['DESKTOP-MQBVAVJ'])
+cassandra = cluster.connect('game')
+
+# Functions
+def insert_game_info(game_id, name, game_type, current_state, world_layout):
+    query = """
+    INSERT INTO GameInfo (game_id, name, type, current_state, world_layout)
+    VALUES (%s, %s, %s, %s, %s)
+    """
+    cassandra.execute(query, (game_id, name, game_type, current_state, world_layout))
+    
+def get_game_info(game_id):
+    query = "SELECT * FROM GameInfo WHERE game_id = %s"
+    row = cassandra.execute(query, (game_id,)).one()
+    return row
+
+def delete_game_info(game_id):
+    query = "DELETE FROM GameInfo WHERE game_id = %s"
+    cassandra.execute(query, (game_id,))
+    
+def game_exists(game_id):
+    query = "SELECT * FROM GameInfo WHERE game_id = %s"
+    result = cassandra.execute(query, (game_id,))
+    
+    # Check if any rows are returned
+    if result.one() is not None:
+        return True
+    else:
+        return False
+    
+# Example usage
+game_id = uuid.uuid4()
+
+# Insert data
+insert_game_info(game_id, 'PUBG', 'Battleground', 'ongoing', '{"map": ["Miramar", "Erangel", "Vekindi"], \
+                 "modes": ["Classic", "Arcade", "Team Deathmatch"]}')
+
+# Query data
+info = get_game_info(game_id)
+print(info)
+
+delete_game_info(game_id)
+
+exist = game_exists(uuid.UUID('e039cb6b-9822-40bb-9ff4-b7c3f3364e87'))
+print(exist)
---
+++ Python Source Code Files/Game_Object_Handler.py
@@ -0,0 +1,38 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+import uuid
+
+# Connect to Cassandra
+cluster = Cluster(['DESKTOP-MQBVAVJ'])
+cassandra = cluster.connect('game')
+
+# Functions
+def insert_game_object(game_id, object_id, obj_type, pos_x, pos_y, pos_z, attributes):
+    query = """
+    INSERT INTO GameObject (game_id, object_id, type, position_x, position_y, position_z, attributes)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    """
+    cassandra.execute(query, (game_id, object_id, obj_type, pos_x, pos_y, pos_z, attributes))
+
+def get_game_objects(game_id):
+    query = "SELECT * FROM GameObject WHERE game_id = %s"
+    rows = cassandra.execute(query, (game_id,))
+    return rows
+
+def delete_game_object(game_id, object_id):
+    query = "DELETE FROM GameObject WHERE game_id = %s AND object_id = %s"
+    cassandra.execute(query, (game_id, object_id))
+    
+# Example usage
+game_id = uuid.uuid4()
+object_id = "AKM"
+
+# Insert data
+insert_game_object(game_id, object_id, 'Weapon', 200, 5, 0, {'ammoCapacity': '30', 'damage': '55', 'recoil' : '+15'})
+
+# Query data
+objects = get_game_objects(game_id)
+for obj in objects:
+    print(obj)
+    
+delete_game_object(game_id, object_id)
